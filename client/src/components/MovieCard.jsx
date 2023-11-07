4003ce5df0e3015584e37125bc29f1e50e8ab702
Daniel Ivanov <80488757+DanielI22@users.noreply.github.com>
2023-11-07 21:00:15 +0200

    Added service and tailwind

---
+++ client/src/components/MovieCard.jsx
@@ -0,0 +1,11 @@
+import styles from "./MovieCard.module.css"
+
+export default function MovieCard() {
+  return (
+    <>
+     <div className={styles.wrapper}>
+      Hello, this is a test div!
+    </div>
+    </>
+  );
+}
