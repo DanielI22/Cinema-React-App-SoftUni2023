9430f44115b7232680e708aec003a0bce646c3bc
Daniel Ivanov <80488757+DanielI22@users.noreply.github.com>
2023-12-09 18:32:45 +0200

    Fixed Movie Details wrong id response

--- client/src/components/MovieDetails/MovieDetails.jsx
+++ client/src/components/MovieDetails/MovieDetails.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import styles from './MovieDetails.module.css';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -13,12 +13,13 @@ import Spinner from '../Spinner/Spinner';
 import ReviewArea from '../ReviewArea/ReviewArea';
 import AuthContext from '../../contexts/authContext';
 import { toast } from 'react-toastify';
-
+import NotFound from '../NotFound/NotFound';
 
 export default function MovieDetails() {
     const { movieId } = useParams();
     const [movie, setMovie] = useState(null);
     const [favourite, setFavourite] = useState(null);
+    const [notFound, setNotFound] = useState(false);
     const { isAuthenticated, userId } = useContext(AuthContext);
 
     useEffect(() => {
@@ -28,15 +29,26 @@ export default function MovieDetails() {
     }, [userId, movieId])
 
     useEffect(() => {
-        movieService.getOne(movieId)
-            .then(result => setMovie(result))
-            .catch(err => toast.error(err))
+        const fetchMovie = async () => {
+            try {
+                const result = await movieService.getOne(movieId)
+                setMovie(result);
+            }
+            catch (err) {
+                if (err.code == 404) {
+                    setNotFound(true);
+                }
+            }
+        }
+        fetchMovie();
     }, [movieId]);
 
+    if(notFound) {
+        return <NotFound />
+    }
     if (!movie) {
         return <Spinner />;
     }
-
     const handleFavouriteClick = async () => {
         if (favourite) {
             await favouriteService.deleteFavourite(favourite._id)
